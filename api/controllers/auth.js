1b6dad49785fc84d1dcf02412e08f145f2c413b6
Tim <13Tim37@gmail.com>
2018-04-08 17:47:22 +0100

    Use bcryptjs instead of bcrypt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 exports.posts = (req, res) => {
     jwt.verify(req.token, process.env.SECRET, (err, authData) => {
