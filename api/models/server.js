55e6e94aab87634a2c941af8542c6c4c5246a618
Tim <13Tim37@gmail.com>
2018-04-08 17:29:09 +0100

    Create servers & list them

--- api/models/server.js
+++ api/models/server.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const Server = mongoose.model('Server', { 
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     address: {
         type: String,
